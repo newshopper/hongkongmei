33dee63215e7977657144cba84c91a8d50faab6a
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2019-12-11 19:01:19 -0500

    get_post_data now gets multiple post data

--- proliferation_pipeline/pipline_functions.py
+++ proliferation_pipeline/pipline_functions.py
@@ -27,61 +27,100 @@ def get_post_comment_ids(post_id):
     else:
         print("something went wrong")
         print("Tried to pull comment ids of: {}".format(post_id))
-        print(request.status_code)
+        print(f'status code: {request.status_code}')
         sys.exit()
 
     return response['data']
     
 
 ################################################################################################
-# Get post data using a specific post id
-# pass a post id such as "6uey5x" that corresponds to the reddit post you are intersted in
+# Get post data using list of post id strings
+# pass a list of unique id strings such as ["6uey5x","degek8","df2rz7"] that corresponds to the reddit post you are intersted in
 # 
-# Use https://api.pushshift.io/reddit/search/submission/ as endpoint and pass 'ids': post_id in header
+# Use https://api.pushshift.io/reddit/search/submission/ as endpoint and pass the list of ids as a parameter
 #
-# Culls for useful data from returned pushift json 
-# Returns author, created_utc, full_link, id, num_comments, num_crossposts, retrieved_on, score,
-# self_text, subreddit, subreddit_subscribers, title, updated_utc, url
+# Returns a list of all the post information
 ###############################################################################################
-def get_post_data(post_id):
+def get_posts_data(post_ids):
     print("Calling get_post_data method")
     
     search_post_endpoint = "https://api.pushshift.io/reddit/search/submission/"    
 
-    params = {
-        'ids': post_id
-    }
+    print(f'Total posts to fetch: {len(post_ids)}')
+    
 
-    request = requests.get(url=search_post_endpoint, params=params)
+    returned_posts = 0
 
-    if request.status_code == 200:
-        response = request.json()
-        data = response['data'][0]
-        data_dict = {
-            "author": data['author'],
-            "created_utc": data['created_utc'],
-            "full_link": data['full_link'],
-            "id": data['id'],
-            "num_comments": data['num_comments'],
-            "num_crossposts": data['num_crossposts'],
-            "retrieved_on": data['retrieved_on'],
-            "score": data['score'],
-            "selftext": data['selftext'],
-            "subreddit": data['subreddit'],
-            "subreddit_subscribers": data['subreddit_subscribers'],
-            "tite": data['title'],
-            "updated_utc": data['updated_utc'],
-            "url": data['url']
+    all_post_data = []
+
+    while returned_posts < len(post_ids): #loop until we have returned data for all posts
+        if len(post_ids) - returned_posts < 500: #if there is less than 500 posts left to return, return what is left to return
+            incoming_posts =  len(post_ids) - returned_posts
+        else:
+            incoming_posts = 500 #else return 500 comments as we build towards returning all posts
+
+        print(f'Fetching posts: {returned_posts} to {returned_posts + incoming_posts}')
+
+        
+        #Isolate the specific comment ids that we are fetching now. We can only fetch a max of 500 at a time
+        fetching_posts = post_ids[returned_posts:returned_posts+incoming_posts]
+        
+        params = {
+            'ids': fetching_posts,
+            'size': 500
         }
-        
-        
-    else:
-        print("something went wrong")
-        print("Tried to pull post data of: {}".format(post_id))
-        print(request.status_code)
-        sys.exit() #kill the program
+        returned_posts = returned_posts + incoming_posts #increment our returned comments for the while loop
 
-    return data_dict
+        request = requests.get(url=search_post_endpoint, params=params)
+
+        if request.status_code == 200:
+            response = request.json()
+            all_post_data = all_post_data + response['data']
+        
+        else:
+            print("something went wrong")
+            print(f"Tried to pull post data from posts: {fetching_posts}")
+            print(f'status code: {request.status_code}')
+            sys.exit()     
+
+    return all_post_data
+    
+#####################    
+# Old code from get_posts_data
+######################
+
+
+    # request = requests.get(url=search_post_endpoint, params=params)
+
+    # if request.status_code == 200:
+    #     response = request.json()
+    #     data = response['data']
+    #     print(data[0])
+        # data_dict = {
+        #     "author": data['author'],
+        #     "created_utc": data['created_utc'],
+        #     "full_link": data['full_link'],
+        #     "id": data['id'],
+        #     "num_comments": data['num_comments'],
+        #     "num_crossposts": data['num_crossposts'],
+        #     "retrieved_on": data['retrieved_on'],
+        #     "score": data['score'],
+        #     "selftext": data['selftext'],
+        #     "subreddit": data['subreddit'],
+        #     "subreddit_subscribers": data['subreddit_subscribers'],
+        #     "tite": data['title'],
+        #     "updated_utc": data['updated_utc'],
+        #     "url": data['url']
+        # }
+        
+        
+    # else:
+    #     print("something went wrong")
+    #     print("Tried to pull post data of: {}".format(post_ids))
+    #     print(f'status code: {request.status_code}')
+    #     sys.exit() #kill the program
+
+    # return 1 #data_dict
     
 
 
@@ -109,7 +148,7 @@ def get_comments_data(post_id,comment_ids):
 
     all_comment_data = [] #the list where we will store all comment data
 
-    while returned_comments < len(comment_ids): #loop until we have returned 
+    while returned_comments < len(comment_ids): #loop until we have returned all comment data
         if len(comment_ids) - returned_comments < 500: #if there is less than 500 comments left to return, return what is left to return
             incoming_comments =  len(comment_ids) - returned_comments 
         else:
@@ -137,25 +176,12 @@ def get_comments_data(post_id,comment_ids):
             print("something went wrong")
             print("Tried to pull comment data from post: {}".format(post_id))
             print(f'Tried to pull comments from indices {returned_comments} to {returned_comments + incoming_comments}')
-            print(request.status_code)
+            print(f'status code: {request.status_code}')
             sys.exit()
         
         
         returned_comments = returned_comments + incoming_comments #increment our returned comments for the while loop
     return all_comment_data # response['data']
-
-
-
-
-######################################################################################
-# return post_ids 
-######################################################################################
-
-
-
-
-
-
 
 
 
